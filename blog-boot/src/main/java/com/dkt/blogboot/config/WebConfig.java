70e1f9a1bafefde89b8baa1eb07cbbefebba9715
doukangtai <2636693728@qq.com>
2021-03-15 23:18:38 +0800

    使用WebMvcConfigurer代替WebMvcConfigurationSupport解决无法自动配置，导致访问static资源404

--- blog-boot/src/main/java/com/dkt/blogboot/config/WebConfig.java
+++ blog-boot/src/main/java/com/dkt/blogboot/config/WebConfig.java
@@ -3,7 +3,7 @@ package com.dkt.blogboot.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 
@@ -12,14 +12,14 @@ import java.io.File;
  * @date 2021/03/15
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
     @Value("${upload-img-path.windows}")
     String uploadImgPathWindows;
     @Value("${upload-img-path.linux}")
     String uploadImgPathLinux;
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String osName = System.getProperty("os.name");
         if ("Windows 10".equals(osName)) {
             registry.addResourceHandler("/upload/image/**").addResourceLocations("file:" + new File(uploadImgPathWindows).getAbsolutePath() + "/");
